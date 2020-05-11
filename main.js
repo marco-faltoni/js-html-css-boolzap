1f429e1d34d32f4a28af3045aa528b6c6017c3fe
Marco <marco.faltoni@outlook.it>
2020-05-11 17:34:31 +0200

    Esercizio Milestone 1

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-$('.input').click(function() {
+$('#message-text').focus(function() {
     $('#mes-but').removeClass('fa-microphone').addClass('fa-paper-plane');
 })
+
+// $('#message-text').blur(function() {
+//     $('#mes-but').removeClass('fa-paper-plane').addClass('fa-microphone');
+// })
 
 $('.microphone').click(function() {
 
     if ($('#mes-but').hasClass('fa-paper-plane')) {
-
         // leggo il testo inserito dall'utente
         var testo_utente = $('#message-text').val();
         console.log(testo_utente);
@@ -20,43 +23,39 @@ $('.microphone').click(function() {
         // appendo il nuovo fumetto risposta utente
         $('.central-tab').append(nuovo_testo_utente);
 
-        // faccio ritornare l'input vuoto al valore iniziale
+        // faccio ritornare l'input vuoto al valore iniziale e ri-assegno la classe iniziale all'icona
         testo_utente = $('#message-text').val('');
-
         $('#mes-but').removeClass('fa-paper-plane').addClass('fa-microphone');
     }
+
 })
 
-$('.input').keydown(function( event ) {
+$('#message-text').keyup(function( event ) {
 
-  if ( event.which == 13 && $('#mes-but').hasClass('fa-paper-plane')) {
-      // leggo il testo inserito dall'utente
-      var testo_utente = $('#message-text').val();
-      console.log(testo_utente);
+    if ( event.which == 13 && $('#mes-but').hasClass('fa-paper-plane')) {
+        // leggo il testo inserito dall'utente
+        var testo_utente = $('#message-text').val();
+        console.log(testo_utente);
 
-      // copio elemento template
-      var nuovo_testo_utente = $('.template .message-right').clone();
-      console.log(nuovo_testo_utente);
+        // copio elemento template
+        var nuovo_testo_utente = $('.template .message-right').clone();
+        console.log(nuovo_testo_utente);
 
-      // inserisco il testo letto dall'input
-      nuovo_testo_utente.find('.h4-light').text(testo_utente);
+        // inserisco il testo letto dall'input
+        nuovo_testo_utente.find('.h4-light').text(testo_utente);
 
-      // appendo il nuovo fumetto risposta utente
-      $('.central-tab').append(nuovo_testo_utente);
+        // appendo il nuovo fumetto risposta utente
+        $('.central-tab').append(nuovo_testo_utente);
 
-      // faccio ritornare l'input vuoto al valore iniziale
-      testo_utente = $('#message-text').val('');
-      $('#mes-but').removeClass('fa-paper-plane').addClass('fa-microphone');
-  }
+        // faccio ritornare l'input vuoto al valore iniziale e ri-assegno la classe iniziale all'icona
+        testo_utente = $('#message-text').val('');
+    }
 });
-
-
-
 
 // $(document).click(function(event) {
 //     var target = $(event.target);
 //
-//     if (!target.hasClass('fa-paper-plane')) && $('.fas').hasClass('fa-paper-plane'){
-//         $('.fas').removeClass('fa-paper-plane').addClass('fa-microphone');
+//     if (!target.hasClass('fa-microphone') && !target.hasClass('fa-paper-plane')) {
+//         $('#mes-but').removeClass('fa-paper-plane').addClass('fa-microphone');
 //     }
 // });
