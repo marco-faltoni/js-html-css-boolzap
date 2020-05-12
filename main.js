19973e4e690b467bf5d8ca1194c01d351ec0d040
Marco <marco.faltoni@outlook.it>
2020-05-12 16:10:36 +0200

    Sistemata al 100% la Milestone 2

--- main.js
+++ main.js
@@ -98,29 +98,6 @@ $("#search-bar").keyup(function() {
     }
 });
 
-$("#filter").keyup(function() {
-
-  // Retrieve the input field text and reset the count to zero
-  var filter = $(this).val(),
-    count = 0;
-
-  // Loop through the comment list
-  $('#results div').each(function() {
-
-
-    // If the list item does not contain the text phrase fade it out
-    if ($(this).text().search(new RegExp(filter, "i")) < 0) {
-      $(this).hide();
-
-      // Show the list item if the phrase matches and increase the count by 1
-    } else {
-      $(this).show();
-      count++;
-    }
-
-  });
-
-});
 
 
 // MEDOTO DELLA SEARCH BAR PIU SEMPLICE E STATICO
